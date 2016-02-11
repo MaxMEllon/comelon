af7bfe2f785b9ff869701d8923c3166176ba188c
MaxMEllon <mozi_kke@excite.co.jp>
2016-02-11 18:38:58 +0900

    Remove unused module

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from '../views/Main.jsx';
-const Nico = require('node-nicovideo-api');
 
 window.onload = function() {
   ReactDOM.render(<Main />, document.getElementById('come'));
