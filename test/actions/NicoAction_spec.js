36954129825715fb4de4e8cbc4c99aa3b0a244a0
MaxMEllon <mozi_kke@excite.co.jp>
2016-03-10 05:21:45 +0900

    Add NicoAction test

--- test/actions/NicoAction_spec.js
+++ test/actions/NicoAction_spec.js
@@ -1,16 +1,26 @@
 'use strict';
 
+require('babel-core/register');
+require('babel-polyfill');
+
 const chai = require('chai');
 const {assert} = require('chai');
 const {sandbox} = require('sinon');
 chai.use(require('sinon-chai'));
 
 describe('NicoAction', () => {
-  let NicoAction, NicoStore = null;
+  let NicoAction, NicoStore, loginAction = null;
 
   before(() => {
     sandbox.create();
     NicoAction = require('../../app/actions/NicoAction');
+    loginAction = () => {
+      let user = {
+        email: process.env.USER_EMAIL,
+        password: process.env.PASSWORD
+      }
+      NicoAction.login(user);
+    }
   });
 
   beforeEach(() => {
@@ -18,7 +28,21 @@ describe('NicoAction', () => {
     NicoStore = require('../../app/stores/NicoStore');
   });
 
-  it('fetch login status', () => {
+  it('try login', () => {
+    loginAction();
+    NicoAction.fetchLoginStatus();
+    assert(NicoStore.isLogin(), true);
+  });
+
+  it('try connect', async () => {
+    NicoAction.fetchLoginStatus();
+    if (! NicoStore.isLogin()) loginAction();
+    NicoAction.connect('nsen/hotaru');
+    let viewer = await NicoStore.getViewer();
+    assert(viewer, true);
+  });
+
+  it('try logout', () => {
     NicoAction.logout();
     NicoAction.fetchLoginStatus();
     assert(NicoStore.isLogin(), false);
