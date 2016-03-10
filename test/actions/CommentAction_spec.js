dc54b0a617e8d2b2131a09f9841660d2b3ee91d5
MaxMEllon <mozi_kke@excite.co.jp>
2016-03-10 09:39:06 +0900

    Fix test

--- test/actions/CommentAction_spec.js
+++ test/actions/CommentAction_spec.js
@@ -10,10 +10,11 @@ const {sandbox} = require('sinon');
 chai.use(require('sinon-chai'));
 
 describe('CommentAction', () => {
-  let NicoAction, NicoStore, CommentAction, CommentStore = null;
+  let NicoAction, NicoStore, CommentAction, CommentStore, Immutable = null;
 
   before(() => {
     sandbox.create();
+    Immutable = require('immutable');
     CommentAction = require('../../app/actions/CommentAction');
     NicoAction = require('../../app/actions/NicoAction');
     NicoStore = require('../../app/stores/NicoStore');
@@ -38,22 +39,29 @@ describe('CommentAction', () => {
     }
     NicoAction.login(user);
     let login = setInterval(() => {
-      if (NicoStore.isLogin()) clearInterval(login);
+      if (NicoStore.isLogin()) {
+        assert(NicoStore.isLogin(), true);
+        let viewer = null;
+        NicoAction.connect('nsen/hotaru');
+        let connect = setInterval(() => {
+          if (viewer !== null) {
+            CommentAction.postComment(`test${Math.random()}`, {mail: '184'});
+            CommentAction.getComment(viewer);
+            let comments = CommentStore.getAllComment();
+            expect(comments).to.not.equal([]);
+            CommentAction.fetchNickname(comments[0]);
+            let sampleComment = Immutable.fromJS({ attr: { user_id: 4197870 } });
+            CommentAction.fetchNickname(sampleComment);
+            let nickname = CommentStore.getNickname(sampleComment.getIn(['attr', 'user_id']));
+            assert(nickname, 'MaxMEllon');
+            clearInterval(connect);
+          }
+          viewer = NicoStore.getViewer();
+        }, 100);
+        clearInterval(login);
+      }
       NicoAction.fetchLoginStatus();
     }, 300);
-    assert(NicoStore.isLogin(), true);
-    let viewer = null;
-    NicoAction.connect('nsen/hotaru');
-    let connect = setInterval(() => {
-      if (viewer !== null) {
-        CommentAction.postComment(`test${Math.random()}`, {mail: '184'});
-        CommentAction.getComment(viewer);
-        let comments = CommentStore.getAllComment();
-        expect(comments).to.not.equal([]);
-        clearInterval(connect);
-      }
-      viewer = NicoStore.getViewer();
-    }, 100);
     done();
   });
 
