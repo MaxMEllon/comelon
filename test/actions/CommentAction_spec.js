dd9495c832ca00c315da75a16e73b295717e8eda
MaxMEllon <mozi_kke@excite.co.jp>
2016-03-10 20:53:48 +0900

    Fix test

--- test/actions/CommentAction_spec.js
+++ test/actions/CommentAction_spec.js
@@ -10,7 +10,7 @@ const {sandbox} = require('sinon');
 chai.use(require('sinon-chai'));
 
 describe('CommentAction', () => {
-  let NicoAction, NicoStore, CommentAction, CommentStore, Immutable = null;
+  let NicoAction, NicoStore, CommentAction, CommentStore, Immutable;
 
   before(() => {
     sandbox.create();
@@ -18,6 +18,11 @@ describe('CommentAction', () => {
     CommentAction = require('../../app/actions/CommentAction');
     NicoAction = require('../../app/actions/NicoAction');
     NicoStore = require('../../app/stores/NicoStore');
+    let user = {
+      email: process.env.USER_EMAIL,
+      password: process.env.PASSWORD
+    }
+    NicoAction.login(user);
   });
 
   beforeEach(() => {
@@ -25,44 +30,43 @@ describe('CommentAction', () => {
     CommentStore = require('../../app/stores/CommentStore');
   });
 
-  it('reset all comment', (done) => {
+  it('reset all comment', done => {
     CommentAction.resetAllComment();
     let comments = CommentStore.getAllComments();
     assert(comments, []);
     done();
   });
 
-  it('get and post comment', (done) => {
-    let user = {
-      email: process.env.USER_EMAIL,
-      password: process.env.PASSWORD
-    }
-    NicoAction.login(user);
-    let login = setInterval(() => {
-      if (NicoStore.isLogin()) {
-        assert(NicoStore.isLogin(), true);
-        let viewer = null;
-        NicoAction.connect('nsen/hotaru');
-        let connect = setInterval(() => {
-          if (viewer !== null) {
-            CommentAction.postComment(`test${Math.random()}`, {mail: '184'});
-            CommentAction.getComment(viewer);
-            let comments = CommentStore.getAllComment();
-            expect(comments).to.not.equal([]);
-            CommentAction.fetchNickname(comments[0]);
-            let sampleComment = Immutable.fromJS({ attr: { user_id: 4197870 } });
-            CommentAction.fetchNickname(sampleComment);
-            let nickname = CommentStore.getNickname(sampleComment.getIn(['attr', 'user_id']));
-            assert(nickname, 'MaxMEllon');
-            clearInterval(connect);
-          }
-          viewer = NicoStore.getViewer();
-        }, 100);
-        clearInterval(login);
+  it('fetch nickname', () => {
+    let sampleComment = Immutable.fromJS({ attr: { user_id: 4197870 } });
+    CommentAction.fetchNickname(sampleComment);
+    let nickname = null;
+    let wait = setInterval(() =>{
+      if (nickname !== null && nickname !== undefined) {
+        expect(nickname).to.be.equal('MaxMEllon');
+        clearInterval(wait);
       }
-      NicoAction.fetchLoginStatus();
-    }, 300);
-    done();
+      nickname = CommentStore.getNickname(sampleComment.getIn(['attr', 'user_id']));
+    }, 100)
+  })
+
+  it('try comment', done => {
+    let wait = setInterval(() => { NicoAction.fetchLoginStatus(); }, 300);
+    let waitConnect = setInterval(() => {
+      if (NicoStore.getViewer() !== null && NicoStore.getViewer() !== null) {
+        CommentAction.getComment(NicoStore.getViewer());
+        clearInterval(waitConnect);
+        done();
+      }
+    }, 500);
+    let loginCallback = () => {
+      if (NicoStore.isLogin()) {
+        expect(NicoStore.isLogin()).to.be.equal(true);
+        NicoAction.connect('nsen/hotaru');
+        clearInterval(wait);
+      }
+    };
+    NicoStore.addChangeListener(loginCallback);
   });
 
   after(() => {
